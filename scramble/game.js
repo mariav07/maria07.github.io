caa7d174ee3fd35618c8d1ff08a925c8a172e1b0
mariav07 <111433940+mariav07@users.noreply.github.com>
2022-09-28 23:46:54 +0000

    Got "nextWord" to work, added some CSS, and started to work on unscrambled

--- scramble/game.js
+++ scramble/game.js
@@ -11,24 +11,21 @@
    - Sometimes users accidently add a space to the end of their words so while they mean to submit the string "pumpkin" they actually submit "pumpkin " -- not the space at the end.
    - Go to the online docs, under strings and look up the .trim() function. 
 
-  TODO 4: Right now you have to reload the page each time. That means the game really can't keep score. Alter the code so that when the player successfully unscrambles a word the game will call the nextWord function.  
+  TODONE 4: Right now you have to reload the page each time. That means the game really can't keep score. Alter the code so that when the player successfully unscrambles a word the game will call the nextWord function.  
     - Move the variable allWords into the nextWord() function
     - Move the code that picks a random word into the nextWord() function
     - Move the code that writes a scrambled word to screen into the nextWord() function
 
-  TODO 4b: Calling nextWord()
+  TODONE 4b: Calling nextWord()
     - Now that we moved the code that selects the next word into a function. We have to call that function.  We will cover Functions in more detail in an upcoming class, but for now, do the following:
       - Call the function nextWord() when the page loads. To do this, uncomment the nextWord() function call at the bottom of this script. 
       - We also want another word called when the user successfully unscrambles a word.  So call the nextWord() function in the code where you think it checks if the users answer is correct. 
 
   TODO 5: Now that we dont have to reload the page to get a new word, we can actually build a game
-  
     - We need to keep track of successfully unscrambled words.  There's a DIV in the HTML called "unscrambled", we need to populate it with the words when they are unscrambled
     - It would be nice if there was a numeric score displayed
     - The UI is BORING! It needs styling and images 
 *********************************************************************/
-
-
 // Our master array of Halloween words
 const allWords = ["Costumes", "Monster", "Disguise", "Ghost", "Witch", "Pumpkin", "Candle", "Zombie", "Frankenstein", "October", "Scarecrow", "Pirate", "Crow", "Cat", "Broomstick", "Vampire", "Prince", "Princess", "Candy", "Werewolf", "Mask", "Spell", "Goblin", "Ghoul", "Alien", "Mummy", "Spooky", "Creepy", "Slimy", "Fangs", "Blood", "Skeleton", "Graveyard", "Party", "Screaming", "Bats", "Skull", "Wicked", "Scary"];
 
@@ -36,29 +33,34 @@ const allWords = ["Costumes", "Monster", "Disguise", "Ghost", "Witch", "Pumpkin"
 //Select a random word from the "words" array and assign it to a variable called "selectedWord";
 let randomIndex = Math.floor(Math.random() * (allWords.length - 1));
 let selectedWord = allWords[randomIndex].toLowerCase();
-
-
-//Scramble the selectedWord and display it inside of a div
-document.getElementById("scrambledWord").innerHTML = selectedWord
-  .split("")
-  .sort(() => Math.random() - 0.5)
-  .join("");
-
-
+let unScrambled = []
 
 // This is a placeholder. You will need to move code into this funcation as part of the assignment.
-function nextWord() { }
+function nextWord() {
+ randomIndex = Math.floor(Math.random() * (allWords.length - 1));
+ selectedWord = allWords[randomIndex].toLowerCase();
+
+//Scramble the selectedWord and display it inside of a div
+ document.getElementById("scrambledWord").innerHTML = selectedWord
+  .split("")
+  .sort(() => Math.random() - 0.5)
+ .join("");
+}
 
 //A function that gets called when the user clicks on the Check button. 
 function check() {
   if (selectedWord === document.getElementById("myWord").value.toLowerCase().trim()) {
-    alert("Correct!")
-  }
+    alert("Correct!");
+unScrambled.push(selectedWord)
+document.getElementById("unscrambled").innerHTML = unScrambled
+}
+  nextWord();
 }
 
 //A function that gets called when the user clicks on the Give Up button.
 function giveUp() {
   document.getElementById("myWord").value = selectedWord;
+  nextWord();
 }
 
-//nextWord();
+nextWord();
