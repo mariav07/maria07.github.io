7ee72c75979277086507da7abfd8d6a0bb678bd5
mariav07 <111433940+mariav07@users.noreply.github.com>
2022-10-15 12:25:48 +0000

    Updated the todo list and made the game over boolean/alert

--- scramble/game.js
+++ scramble/game.js
@@ -3,7 +3,7 @@
     - With the first letter of every word capitalized, it makes it too easy to play. When scrambled the word should either be all upper or lower case
     - HINT: the variable that hold the word that will be scrambled is called "selectedWord"
 
-  TODONE 2: JS is case sensitive. -- Remember how I showed  "ken" and "Ken" are notw3 the same.
+  TODONE 2: JS is case sensitive. -- Remember how I showed  "ken" and "Ken" are not the same.
     - When checking your answer, the comparision is case sensitive. You should make sure that the cases are the same.
     - To do this, locate where the code is COMPARING the selected word to the value in the form field and set them both to either upper or lower case
 
@@ -21,31 +21,50 @@
       - Call the function nextWord() when the page loads. To do this, uncomment the nextWord() function call at the bottom of this script. 
       - We also want another word called when the user successfully unscrambles a word.  So call the nextWord() function in the code where you think it checks if the users answer is correct. 
 
-  TODONE 5: Now that we dont have to reload the page to get a new word, we can actually build a game
-    - We need to keep track of successfully unscrambled words.  There's a DIV in the HTML called "unscrambled", we need to populate it with the words when they are unscrambled
-    - It would be nice if there was a numeric score displayed
+  --- Now that we dont have to reload the page to get a new word, we can actually build a game --- 
+  
+  TODONE 5: 
+    - We need to keep track of successfully unscrambled words.  There's a DIV in the HTML called "unscrambled", we need to populate it with the words when they are unscrambled.
+    - This is how you SET/GET the HTML of the "unscrambled" div:  document.getElementById("unscrambled").innerHTML="";
+    - Think back to class when I showed you how we can add strings together.  That is what you want to do here.  You want to combine the existing string value of the "unscrambled" div with the string value of the unscrambled word. 
+    
+  TODONE 6: The game should end when the user unscrambles a word incorrectly
+    - Create a new variable to track if the game is over. (what type should that be, number, string, bool?)
+    - Initialize that new variable by setting it to false when the page loads
+    - You should set that variable to true if the user gives up or fails to unscramble a word. 
+    - HINT Look in the CHECK function, there is a conditional that checks IF the word is correct. Now you need to add an ELSE and in that else condition we want to:
+
+  TODONE 7:
+    - We should give the player some sort of message when the game is over and maybe include how many words they successfully unscrambled. Something like:
+      - Game Over! You unscrabled __ words!
+
+  TODONE 8:
     - The UI is BORING! It needs styling and images 
 *********************************************************************/
-// Our master array of Halloween words
+
+let selectedWord = "";
+
+// Our master array of Halloween words. Note how it is declared as a CONST. That's becuase we don't want anything changing these values.
 const allWords = ["Costumes", "Monster", "Disguise", "Ghost", "Witch", "Pumpkin", "Candle", "Zombie", "Frankenstein", "October", "Scarecrow", "Pirate", "Crow", "Cat", "Broomstick", "Vampire", "Prince", "Princess", "Candy", "Werewolf", "Mask", "Spell", "Goblin", "Ghoul", "Alien", "Mummy", "Spooky", "Creepy", "Slimy", "Fangs", "Blood", "Skeleton", "Graveyard", "Party", "Screaming", "Bats", "Skull", "Wicked", "Scary"];
 
 
-//Select a random word from the "words" array and assign it to a variable called "selectedWord";
-let randomIndex = Math.floor(Math.random() * (allWords.length - 1));
-let selectedWord = allWords[randomIndex].toLowerCase();
+//Select a random word from the "allWords" array and assign it to a variable called "selectedWord";
+let randomIndex = Math.floor(Math.random() * (allWords.length - 1)); //Don't worry about this math, but it will select a random number between 0 and the last index of the allWords array.
+selectedWord = allWords[randomIndex].toLowerCase();
 let list = [];
 let number = 0;
+let game_over = false;
 
-// This is a placeholder. You will need to move code into this funcation as part of the assignment.
+// This is a placeholder. You will need to move code into this funcation as part of the assignment. HINT -- The code you want to move into this function is above. :)
 function nextWord() {
  randomIndex = Math.floor(Math.random() * (allWords.length - 1));
- selectedWord = allWords[randomIndex].toLowerCase();
+ selectedWord = allWords[randomIndex].toLowerCase(); //Use that random index in allWords and set that to a new variable that will hold our selectedWord.
 
-//Scramble the selectedWord and display it inside of a div
+//Scramble the selectedWord and display it inside of a div called "scrambledWord"
  document.getElementById("scrambledWord").innerHTML = selectedWord
-  .split("")
-  .sort(() => Math.random() - 0.5)
- .join("");
+  .split("") // This will split the word into an array of letters
+  .sort(() => Math.random() - 0.5) // This will ramdomly sort the array of letters
+ .join(""); // Now we want to join all those array elements back together into a string
 }
 
 //A function that gets called when the user clicks on the Check button. 
@@ -56,15 +75,19 @@ function check() {
     document.getElementById("unscrambled").innerHTML = list;
     number = (list.length);
     document.getElementById("unscrambledNumber").innerHTML = number;
-  }
   nextWord();
+  } else {
+    game_over = true;
+    alert("Game over! You unscrambled " + number + " words!")
+  }
 }
 
 
 //A function that gets called when the user clicks on the Give Up button.
 function giveUp() {
   document.getElementById("myWord").value = selectedWord;
-  nextWord();
+  game_over = true;
+   alert("Game over! You unscrambled " + number + " words!");
 }
 
 
